a3ed37c479c170e6d17fee956d57be15eb919b73
Danilo Vitoriano <vitoriano08@gmail.com>
2016-04-28 10:39:16 -0300

    Create preventDefault.js

---
+++ preventDefault.js
@@ -0,0 +1,21 @@
+$("a").click(function() {
+   $("body").append($(this).attr("href"));
+   return false;
+}
+
+$("a").click(function(e) {
+   $("body").append($(this).attr("href"));
+   e.preventDefault();
+}
+
+// function return false
+
+function() {
+  return false;
+}
+
+// IS EQUAL TO
+
+function(e) {
+  e.preventDefault();
+  
